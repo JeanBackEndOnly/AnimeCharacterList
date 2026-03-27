3853a14718f0c2da51a19d68b21388d6220748ee
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2026-03-27 14:56:37 +0800

    qweqw
    qwe

---
+++ database/migrations/2026_03_23_064958_create_character_info_db.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('character_info_db', function (Blueprint $table) {
+            $table->id();
+            $table->string('anime');
+            $table->string('name');
+            $table->string('icon');
+            $table->text('about');
+            $table->text('goals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('character_info_db');
+    }
+};
